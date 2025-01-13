50e240882764767bb95695d9d5b2b4b94222fed4
Vaibhav Verma <144448236+vverma01232@users.noreply.github.com>
2025-01-13 16:59:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def main():
     
     # Access workspace and project
     workspace = rf.workspace("aetos")
-    project = workspace.project("thermal1-copy")
+    project = workspace.project("ptz-copy-1")
     version = project.version(2)
     dataset = version.download("yolov11")
     
     print("Dataset downloaded successfully to:", datasets_path)
-    # print("Yaml localtion: ", data=datasets_path+"datasets/thermal1-copy/data.yaml")
+    # print("Yaml localtion: ", data=datasets_path+"datasets/ptz-copy-1/data.yaml")
 
     print("Training model Now........................................................")
-    model = YOLO("yolo11x.pt")  # load a pretrained model (recommended for training)# Train the model with 2 GPUs
-    results = model.train(data=datasets_path+"/thermal1-copy-2/data.yaml", epochs=10, imgsz=640, plots=True, workers=2, batch_size=4)
+    # model = YOLO("yolo11x.pt")  # load a pretrained model (recommended for training)# Train the model with 2 GPUs
+    # results = model.train(data=datasets_path+"/thermal1-copy-2/data.yaml", epochs=10, imgsz=640, plots=True, workers=2, batch_size=4)
 
 if __name__ == "__main__":
     main()
